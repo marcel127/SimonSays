77cf84cee853ebcb9899a022c621ac1698f452ea
marcel127 <marcel.bin27@gmail.com>
2021-02-22 20:28:45 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import SimonSaysMain from './SimonSaysMain';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => SimonSaysMain);
