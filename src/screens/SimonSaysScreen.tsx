b625a3c719db1613b2535ab671a0d3fbcb318b56
marcel127 <marcel.bin27@gmail.com>
2021-02-23 23:09:39 +0200

    work flow

--- src/screens/SimonSaysScreen.tsx
+++ src/screens/SimonSaysScreen.tsx
@@ -1,12 +1,20 @@
 
 
-import React, { useEffect, useState } from 'react';
-import {SafeAreaView, StyleSheet, ScrollView, View, Text} from 'react-native';
+import React, { useEffect, useRef, useState } from 'react';
+import {SafeAreaView, StyleSheet, ScrollView, View, Text, Alert} from 'react-native';
 import { ParamList } from '../utils/ParamList';
 import { StackNavigationProp } from '@react-navigation/stack';
 import { CurrentRenderContext, RouteProp } from '@react-navigation/native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
-import { ColorMap, SIMON_TURN, USER_TURN } from '../utils/Constant';
+import { GREEN,YELLOW,BLUE,RED, SIMON_TURN, USER_TURN } from '../utils/Constant';
+import { Store } from 'redux';
+import SimonColor from '../redux/reducers';
+import {  useDispatch, useSelector } from 'react-redux';
+import { SimonColorChoose } from '../redux/actions';
+
+import Sound from 'react-native-sound';
+
+
 
 
 
@@ -30,60 +38,141 @@ const initialState:State ={
   currentStep : 0,
   isStarted : false,
 }
+Sound.setCategory("Playback");
 
 const SimonSaysScreen:React.FC<Props> = ({navigation, route}) => {
 
+  const blueRef = useRef<TouchableOpacity>(null);
+  const yellowRef = useRef<TouchableOpacity>(null); 
+  const redRef = useRef<TouchableOpacity>(null);
+  const greenRef = useRef<TouchableOpacity>(null);
+
+  const colorsLUT = 
+                    [
+                      {colorRef : greenRef , simonSound: new Sound('green_simon.wav', Sound.MAIN_BUNDLE, (error) => {
+                        if (error) {
+                            console.log('failed to load the sound', error);
+                        }
+                    }) , playerSound : new Sound('../sounds/green_user.wav') },
+                      {colorRef : blueRef , simonSound: new Sound('../sounds/blue_simon.wav') , playerSound : new Sound('../sounds/blue_user.wav')  },
+                      {colorRef : yellowRef , simonSound: new Sound('../sounds/yellow_simon.wav') , playerSound : new Sound('../sounds/yellow_simon.wav') },
+                      {colorRef : redRef , simonSound: new Sound('../sounds/red_simon.wav') , playerSound : new Sound('../sounds/red_user.wav') },
+                    ]
+	
   const [state,setState] = useState<State>(initialState)
-  const [currentColor, setCurrentColor] = useState<number>(0)
-  // Math.floor(Math.random() * 4)
+  
+
 
   useEffect(() => {
+    showSimonChoose();
+  },[state.stepsArray])
+
+  useEffect(() => {
+
+    const checkLastStep = async () => {
+      if(state.isStarted && state.currentStep === state.stepsArray.length){
+        setState(prevState => ({...prevState,score : state.score + 1, playerTurn : SIMON_TURN}))
+        await new Promise(resolve => setTimeout(resolve, 1000));
+        
+        simonTurn();
+      }
+    }
+
+    checkLastStep();
+  },[state.currentStep])
+
+  const isCorrectPress = (sequence : number) => {
+    colorsLUT[sequence].playerSound.play()
+
+    if(state.stepsArray[state.currentStep] !== sequence){
+      Alert.alert("you lose")
+    }
+
+    else{
+      setState(prevState => ({...prevState,currentStep : state.currentStep + 1}))
+    }
+  }
+
+  const simonTurn = async () => { 
+    console.log(colorsLUT[0].playerSound)
+
+      setState(prevState => ({
+        ...prevState,
+        stepsArray : [...state.stepsArray, Math.floor(Math.random() * 4)],
+      }))
+    
+  }
+
+  const showSimonChoose = async () =>{
     console.log(state.stepsArray)
-  },[currentColor])
 
-  const isLastStep = () => {
-
-  }
-
-  const isCorrectPress = () => {
-
-  }
-
-  const simonChoose = async () => { 
-    setState(prevState => ({
-      ...prevState,
-      stepsArray : [...state.stepsArray, Math.floor(Math.random() * 4)],
-      // intervalNumber : setTimeout(() => showSimonChoose(),1000 ),
-      score : state.currentStep + 1,
-      currentStep : state.currentStep + 1,
-    }))
-    setCurrentColor(state.stepsArray[state.stepsArray.length - 1])
-    console.log(state.stepsArray)
-    console.log("current step" + state.currentStep)
-    showSimonChoose();   
-  }
-
-  const showSimonChoose = () =>{
- 
+    for(var i = 0; i < state.stepsArray.length ; ++i){
+        colorsLUT[state.stepsArray[i]].simonSound.play((success) => {
+          if (success) {
+            console.log('successfully finished playing');
+          } else {
+            console.log('playback failed due to audio decoding errors');
+          }
+        });
+        colorsLUT[state.stepsArray[i]].colorRef.current?.setOpacityTo(0.1,1000);
+        await new Promise(resolve => setTimeout(resolve, 1000));
+        colorsLUT[state.stepsArray[i]].colorRef.current?.setOpacityTo(1,1000);
+        await new Promise(resolve => setTimeout(resolve, 1000));
+    }
+    
+    setState(prevState => ({...prevState, playerTurn : USER_TURN, currentStep : 0}))
   }
 
   return (
     <View style={styles.container}>
-      <TouchableOpacity style={[styles.StartButtonStyle, {display : state.isStarted ? 'none' : 'flex'}]} onPress={() => simonChoose()}>
-        <Text style={{alignSelf:'center', fontSize:20,}}>start</Text>
+      
+      <TouchableOpacity style={[styles.StartButtonStyle]}
+          disabled={state.isStarted}
+          onPress={() => {
+              setState(prevState => ({...prevState, isStarted : true}))
+              simonTurn()}
+          }>
+        <Text style={{alignSelf:'center', fontSize:20}}>{!state.isStarted ? "start" : state.playerTurn === SIMON_TURN ? "Simon say...." : "Your turn"}</Text>
       </TouchableOpacity>
       
-      {ColorMap.map(data => (
-          <TouchableOpacity
-              key={data.id}
-              style={[styles.ButtonStyle,{backgroundColor : data.color ,opacity: currentColor === data.id ? 0.5 : 1 }]}
-              // disabled={ state.playerTurn !== USER_TURN }
-              onPress={ () => {   
-
-                              }}>
+      <View style={{backgroundColor:'black',justifyContent : 'center',height:'60%',width:"70%",alignItems:'center',borderRadius:20}}>
+        <View style={{flexDirection : 'row'}}>
+            <TouchableOpacity
+                ref = {greenRef}
+                disabled = {state.playerTurn === SIMON_TURN}
+                style={[styles.ButtonStyle,{backgroundColor : "green" }]}
+                onPress={ () => isCorrectPress(0)  }>
             </TouchableOpacity>
-      ))}
+                  
+            <TouchableOpacity
+                ref = {blueRef}
+                disabled = {state.playerTurn === SIMON_TURN}
+                style={[styles.ButtonStyle,{backgroundColor : "blue" }]}
+                onPress={ () => isCorrectPress(1)  }>
 
+            </TouchableOpacity>
+        </View>
+
+        <View style={{flexDirection : 'row'}}>
+            <TouchableOpacity
+                ref = {yellowRef}
+                disabled = {state.playerTurn === SIMON_TURN}
+                style={[styles.ButtonStyle,{backgroundColor : "yellow" }]}
+                onPress={ () => isCorrectPress(2)  }>
+
+            </TouchableOpacity>
+
+            <TouchableOpacity
+                ref = {redRef}
+                disabled = {state.playerTurn === SIMON_TURN}
+                style={[styles.ButtonStyle,{backgroundColor : "red" }]}
+                onPress={ () => isCorrectPress(3)  }>
+
+            </TouchableOpacity>
+        </View>
+      </View>
+
+      <Text style={{fontSize:30,marginTop:10}}>your score is : {state.score}</Text>
     </View>
   );
 };
@@ -94,16 +183,17 @@ const styles = StyleSheet.create({
   },
   StartButtonStyle : {
     marginTop : 20,
+    marginBottom:50,
     backgroundColor:'cyan',
-    width:70
+    width:"100%"
   },
   ButtonStyle : {
     height : 100,
     width : 100,
-    marginTop : 30,
+    marginVertical : 10,
+    marginHorizontal : 10,
+    borderRadius: 20,
   }
-
-
 });
 
 export default SimonSaysScreen;
--- src/utils/Constant.ts
+++ src/utils/Constant.ts
@@ -1,5 +1,9 @@
 export const SIMON_TURN = 0
 export const USER_TURN = 1
+export const GREEN  = 0
+export const BLUE = 1
+export const YELLOW = 2
+export const RED = 3
 
 interface ColorProps  {
     id : number
@@ -8,33 +12,33 @@ interface ColorProps  {
     soundUser : string 
 }
 
-export const ColorMap = [
-        {
-            id : 0,
-            color : 'green',
-            soundSimon : '',
-            soundUser : ''
-        },
-        {
-            id : 1,
-            color : 'yellow',
-            soundSimon : '',
-            soundUser : ''
+// export const ColorMap = [
+//         {
+//             id : 0,
+//             color : 'green',
+//             soundSimon : '',
+//             soundUser : ''
+//         },
+//         {
+//             id : 1,
+//             color : 'yellow',
+//             soundSimon : '',
+//             soundUser : ''
           
-        },
-        {
-            id : 2,
-            color : 'red',
-            soundSimon : '',
-            soundUser : ''
+//         },
+//         {
+//             id : 2,
+//             color : 'red',
+//             soundSimon : '',
+//             soundUser : ''
           
-        },
-        {
-            id : 3,
-            color : 'blue',
-            soundSimon : '',
-            soundUser : ''
-        },
-]
+//         },
+//         {
+//             id : 3,
+//             color : 'blue',
+//             soundSimon : '',
+//             soundUser : ''
+//         },
+// ]
 
   
